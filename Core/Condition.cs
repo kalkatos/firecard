bdf13d454d512bd73c3f24e9db844675c7749d0e
Alex Kalkatos <alexcsts@gmail.com>
2023-11-15 00:14:14 -0300

    CSharpGame sub & Condition initial implementation

--- Core/Condition.cs
+++ Core/Condition.cs
@@ -3,8 +3,24 @@ using System;
 namespace Kalkatos.Firecard.Core
 {
     [Serializable]
-    public class Condition
+    public class Condition : IValueGetter<bool>
     {
+        private bool value;
+        private Condition sub;
+        private Condition and;
+        private Condition or;
 
+        public bool Value => GetValue();
+
+        public bool GetValue ()
+        {
+            if (sub != null)
+                return sub.Value;
+            if (and != null)
+                return value && and.Value;
+            if (or != null)
+                return value || or.Value;
+            return value;
+        }
     }
 }
