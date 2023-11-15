e37c71d87a56a95ffe21cfee07ead67b821901e0
Alex Kalkatos <alexcsts@gmail.com>
2023-11-15 01:05:02 -0300

    Condition value and evaluate WIP

--- Core/Condition.cs
+++ Core/Condition.cs
@@ -5,22 +5,65 @@ namespace Kalkatos.Firecard.Core
     [Serializable]
     public class Condition : IValueGetter<bool>
     {
+        public string Left;
+        public Operation Operator;
+        public string Right;
+        public Condition Sub;
+        public Condition And;
+        public Condition Or;
+
         private bool value;
-        private Condition sub;
-        private Condition and;
-        private Condition or;
+
+        public enum Operation
+        {
+            Equals = 0,
+            NotEquals = 1,
+            LessThan = 2,
+            LessOrEquals = 3,
+            GreaterThan = 4,
+            GreaterOrEquals = 5,
+        }
+        public static string[] Operators = new string[] { "=", "!=", "<", "<=", ">", ">=" };
 
         public bool Value => GetValue();
+        public string Face => Left + Operators[(int)Operator] + Right;
 
         public bool GetValue ()
         {
-            if (sub != null)
-                return sub.Value;
-            if (and != null)
-                return value && and.Value;
-            if (or != null)
-                return value || or.Value;
+            if (Sub != null)
+                value = Sub.Value;
+            if (And != null)
+                return value && And.Value;
+            if (Or != null)
+                return value || Or.Value;
             return value;
+        }
+
+        public void Evaluate (MatchState matchState)
+        {
+            And?.Evaluate(matchState);
+            Or?.Evaluate(matchState);
+            if (Sub != null)
+            {
+                Sub.Evaluate(matchState);
+                value = Sub.value;
+                return;
+            }
+            // TODO Set own value based on what matchState contains
+        }
+
+        public override string ToString ()
+        {
+            string result;
+            if (Sub != null)
+                result = $"({Sub})";
+            else
+                result = Face;
+            if (And != null)
+                result += $" AND {And}";
+            if (Or != null)
+                result += $" OR {Or}";
+            return result;
         }
     }
 }
