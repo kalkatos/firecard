94e9df6fa2c4ed59512a997879e6f241af81a57f
Alex Kalkatos <alexcsts@gmail.com>
2023-11-24 10:40:50 -0300

    Field getter implementation

--- Getter/FieldGetter.cs
+++ Getter/FieldGetter.cs
@@ -1,19 +1,54 @@
-﻿using System;
+﻿using Kalkatos.Firecard.Core;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
 
 namespace Kalkatos.Firecard.Utility
 {
     [Serializable]
     public class FieldGetter : Getter
     {
-        public override object Get ()
+        [JsonProperty]
+        internal string fieldName;
+        [JsonProperty]
+        internal StringGetter fieldNameGetter;
+        [JsonProperty]
+        internal CardGetter cardGetter;
+
+        private string FieldName => (string.IsNullOrEmpty(fieldName) ? fieldNameGetter?.GetString() : fieldName);
+
+        public FieldGetter () { }
+
+        public FieldGetter (string fieldName, CardGetter cardGetter)
         {
-            throw new NotImplementedException();
+            this.fieldName = fieldName;
+            this.cardGetter = cardGetter;
         }
 
-        public bool Evaluate ()
+        public override object Get ()
         {
-            // TODO
-            return false;
+            if (string.IsNullOrEmpty(FieldName))
+                return null;
+            List<Card> cards = cardGetter.GetCards();
+            if (cards.Count == 0)
+                return null;
+            return cards[0].GetFieldValue(FieldName);
+        }
+
+        public string GetStringValue ()
+        {
+            List<Card> cards = cardGetter.GetCards();
+            if (cards.Count == 0)
+                return null;
+            return cards[0].GetTextFieldValue(FieldName);
+        }
+
+        public float GetNumberValue ()
+        {
+            List<Card> cards = cardGetter.GetCards();
+            if (cards.Count == 0)
+                return float.NaN;
+            return cards[0].GetNumericFieldValue(FieldName);
         }
     }
 }
